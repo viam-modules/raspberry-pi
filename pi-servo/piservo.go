b8dcba30258fc1bbbb9fc6ff71eb826542d0ed27
cloudui <eric.chen@viam.com>
2024-07-16 13:52:09 -0400

    Create model for pi-servo

--- pi-servo/piservo.go
+++ pi-servo/piservo.go
@@ -1,2 +1,6 @@
-// Servo implementation here 
+// Implementation servo component on Raspberry Pi
+package rpiservo
 
+import "go.viam.com/rdk/resource"
+
+var Model = resource.NewModel("viam", "raspberry-pi", "rpi-servo")
