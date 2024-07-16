dcea41b2007926f6eb4ea6789d69a6107f374aa3
cloudui <eric.chen@viam.com>
2024-07-16 15:41:01 -0400

    Fix spacing

--- rpi/rpi.go
+++ rpi/rpi.go
@@ -8,7 +8,7 @@ package rpi
 		  For software PWM, we currently support the default sample rate of
 		  5 microseconds, which supports the following 18 frequencies (Hz):
 		  8000  4000  2000 1600 1000  800  500  400  320
-          250   200   160  100   80   50   40   20   10
+		  250   200   160  100   80   50   40   20   10
 		  Details on this can be found here -> https://abyz.me.uk/rpi/pigpio/pdif2.html#set_PWM_frequency
 */
 
