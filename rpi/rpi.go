6f320587d76735abadad6d0dfd079a1908e725b4
cloudui <eric.chen@viam.com>
2024-07-16 15:25:58 -0400

    Add module descriptions

--- rpi/rpi.go
+++ rpi/rpi.go
@@ -1,6 +1,17 @@
 // Package rpi implements raspberry pi board
 package rpi
 
+/*
+	This driver contains various functionalities of raspberry pi board using the
+	pigpio daemon library (https://abyz.me.uk/rpi/pigpio/pdif2.html).
+	NOTE: This driver only supports software PWM functionality of raspberry pi.
+		  For software PWM, we currently support the default sample rate of
+		  5 microseconds, which supports the following 18 frequencies (Hz):
+		  8000  4000  2000 1600 1000  800  500  400  320
+          250   200   160  100   80   50   40   20   10
+		  Details on this can be found here -> https://abyz.me.uk/rpi/pigpio/pdif2.html#set_PWM_frequency
+*/
+
 import "go.viam.com/rdk/resource"
 
 var Model = resource.NewModel("viam", "raspberry-pi", "rpi")
