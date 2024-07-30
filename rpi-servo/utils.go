52673e9b1a60536e26cd02f8e647ccb1a311dcfd
cloudui <eric.chen@viam.com>
2024-07-30 15:39:19 -0400

    Move pin set from validateAndSetConfig to initializePiServo

--- rpi-servo/utils.go
+++ rpi-servo/utils.go
@@ -28,7 +28,5 @@ func (s *piPigpioServo) validateAndSetConfiguration(conf *ServoConfig) error {
 		return errors.New("maxRotation is less than maximum")
 	}
 
-	s.pinname = conf.Pin
-
 	return nil
 }
