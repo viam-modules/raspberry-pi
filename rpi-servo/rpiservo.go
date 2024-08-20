c2c120f2857dadd0956facebf53d409d45bee39b
Susmita Sanyal <susmita@viam.com>
2024-08-20 13:25:43 -0400

    comments

--- rpi-servo/rpiservo.go
+++ rpi-servo/rpiservo.go
@@ -203,17 +203,6 @@ func (s *piPigpioServo) IsMoving(ctx context.Context) (bool, error) {
 
 // Close gracefully stops any ongoing operations and disconnects from the pigpio daemon.
 func (s *piPigpioServo) Close(ctx context.Context) error {
-	s.logger.Debug("Attempting to gracefully stop ongoing operations")
-	_, done := s.opMgr.New(ctx)
-	defer done()
-
-	// Set pulse width to 0 to stop the servo
-	err := s.setServoPulseWidth(0)
-	if err != nil {
-		s.logger.Errorf("Failed to set pulse width to 0: %v", err)
-		return err
-	}
-
 	s.logger.Debug("Stopping pigpio connection")
 	C.pigpio_stop(s.piID)
 
