c697795f42e84e0d36195a76c03b992d36f57b99
Susmita Sanyal <susmita@viam.com>
2024-08-21 17:07:51 -0400

    adding pigpio to run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #Install pigpio packets 
-sudo apt-get install -qqy libpigpio-dev libpigpiod-if-dev
+sudo apt-get install -qqy libpigpio-dev libpigpiod-if-dev pigpio
 
-exec ./bin/raspberry-pi
+exec ./bin/raspberry-pi $1
