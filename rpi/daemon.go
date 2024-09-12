de39cfa54d1315e111e80548f5e90b14ce874ef6
Susmita Sanyal <susmita@viam.com>
2024-09-12 16:06:41 -0400

    feedback

--- rpi/daemon.go
+++ rpi/daemon.go
@@ -8,16 +8,18 @@ import (
 	"go.viam.com/rdk/logging"
 )
 
+// This is a constant timeout for starting and stopping the pigpio daemon
+var CTX_TIMEOUT = 10 * time.Second
+
 // startPigpiod tries to start the pigpiod daemon.
 // It returns an error if the daemon fails to start.
 func startPigpiod(logger logging.Logger) error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), CTX_TIMEOUT)
 	defer cancel()
 
 	// check if pigpio is active
 	statusCmd := exec.CommandContext(ctx, "systemctl", "is-active", "--quiet", "pigpiod")
-	err := statusCmd.Run()
-	if err != nil {
+	if err := statusCmd.Run(); err != nil {
 		startCmd := exec.CommandContext(ctx, "systemctl", "restart", "pigpiod")
 		if err := startCmd.Run(); err != nil {
 			logger.Debug("failed to restart pigpiod")
@@ -25,12 +27,12 @@ func startPigpiod(logger logging.Logger) error {
 		}
 	}
 	logger.Info("pigpiod is already running")
-	return err
+	return nil
 }
 
 // stopPigpiod stops the pigpiod daemon.
 func stopPigpiod() error {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), CTX_TIMEOUT)
 	defer cancel()
 
 	stopCmd := exec.CommandContext(ctx, "systemctl", "stop", "pigpiod")
