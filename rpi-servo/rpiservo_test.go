4a1e5554622637da85126e3d5d35e63d7d6b6e9e
Susmita Sanyal <susmita@viam.com>
2024-09-12 12:09:23 -0400

    adding sleep between tests

--- rpi-servo/rpiservo_test.go
+++ rpi-servo/rpiservo_test.go
@@ -3,6 +3,7 @@ package rpiservo
 import (
 	"context"
 	"testing"
+	"time"
 
 	"raspberry-pi/rpi"
 
@@ -83,6 +84,8 @@ func TestConstructor(t *testing.T) {
 }
 
 func TestInitializationFunctions(t *testing.T) {
+
+	time.sleep(10 * time.Millisecond)
 	ctx := context.Background()
 
 	p := createDummyBoard(t, ctx)
