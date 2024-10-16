cc7497653c9da4c465caf08af916a4668ba8e1a5
Susmita Sanyal <susmita@viam.com>
2024-10-16 13:57:22 -0400

    updating path

--- run.sh
+++ run.sh
@@ -27,10 +27,10 @@ ARCH=$(uname -m)
 
 if [ "$ARCH" = "aarch64" ]; then
     # 64-bit ARM architecture
-    exec ./bin/raspberry-pi/arm64 "$@"
-elif [ "$ARCH" = "armv7l" ] || [ "$ARCH" = "armv6l" ]; then
+    exec ./bin/raspberry-pi-arm64 "$@"
+elif [[ "$ARCH" =~ armv[0-9]+l ]]; then then
     # 32-bit ARM architecture
-    exec ./bin/raspberry-pi/arm32 "$@"
+    exec ./bin/raspberry-pi-arm "$@"
 else
     echo "Unsupported architecture: $ARCH" >&2
     exit 1
